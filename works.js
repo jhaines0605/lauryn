a58d76675472b3368be35f6862fd05e80bacfa9d
jhaines0605 <jhaines0605@gmail.com>
2026-08-31 15:43:01 -0700

    Update works.js from studio admin

--- works.js
+++ works.js
@@ -72,7 +72,7 @@ window.WORKS = {
     "newsletterField": "email_address",
     "newsletterHead": "First look at new work",
     "newsletterBody": "New paintings go out to this list before they go anywhere else. No spam, just the work.",
-    "updated": "2026-08-31T22:39:56.620Z"
+    "updated": "2026-08-31T22:43:02.995Z"
   },
   "pieces": [
     {
